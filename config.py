7380c6952950becb15be86e7977e8d41aeca92c3
marcsilva <marciliodevjava@gmail.com>
2024-01-16 19:37:49 -0300

    projeto nos finalmentes

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+SECRET_KEY = 'alura'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}:{port}/{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='1234567890',
+        servidor='127.0.0.1',
+        port='3306',
+        database='jogoteca'
+    )
+
+UPLOAD_PATH = r'.\uploads'
