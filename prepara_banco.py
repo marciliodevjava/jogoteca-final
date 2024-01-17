98634ea257ec76e524617a484da4053a481184f4
marcsilva <marciliodevjava@gmail.com>
2024-01-17 17:27:32 -0300

    Update prepara_banco.py

--- prepara_banco.py
+++ prepara_banco.py
@@ -1,18 +1,20 @@
 import mysql.connector
+from flask_bcrypt import generate_password_hash
 from mysql.connector import errorcode
 
 print("Conectando...")
 try:
-      conn = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='admin'
-      )
+    conn = mysql.connector.connect(
+        host='127.0.0.1',
+        port='3306',
+        user='root',
+        password='1234567890'
+    )
 except mysql.connector.Error as err:
-      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print('Existe algo errado no nome de usuário ou senha')
-      else:
-            print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Existe algo errado no nome de usuário ou senha')
+    else:
+        print(err)
 
 cursor = conn.cursor()
 
@@ -42,25 +44,26 @@ TABLES['Usuarios'] = ('''
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
 for tabela_nome in TABLES:
-      tabela_sql = TABLES[tabela_nome]
-      try:
-            print('Criando tabela {}:'.format(tabela_nome), end=' ')
-            cursor.execute(tabela_sql)
-      except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                  print('Já existe')
-            else:
-                  print(err.msg)
-      else:
-            print('OK')
-
+    tabela_sql = TABLES[tabela_nome]
+    try:
+        print('Criando tabela {}:'.format(tabela_nome), end=' ')
+        cursor.execute(tabela_sql)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print('Já existe')
+        else:
+            print(err.msg)
+    else:
+        print('OK')
 
 # inserindo usuarios
 usuario_sql = 'INSERT INTO usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
 usuarios = [
-      ("Bruno Divino", "BD", "alohomora"),
-      ("Camila Ferreira", "Mila", "paozinho"),
-      ("Guilherme Louro", "Cake", "python_eh_vida")
+    ("Bruno Divino", "BD", generate_password_hash("alohomora").decode('utf-8')),
+    ("Camila Ferreira", "Mila", generate_password_hash("paozinho").decode('utf-8')),
+    ("Administrador", "root", generate_password_hash("123").decode('utf-8')),
+    ("Marcilio Zanatta", "marcilio", generate_password_hash("123456789").decode('utf-8')),
+    ("Alessandra Luiza", "aleluiza", generate_password_hash("123").decode('utf-8'))
 ]
 cursor.executemany(usuario_sql, usuarios)
 
@@ -72,12 +75,12 @@ for user in cursor.fetchall():
 # inserindo jogos
 jogos_sql = 'INSERT INTO jogos (nome, categoria, console) VALUES (%s, %s, %s)'
 jogos = [
-      ('Tetris', 'Puzzle', 'Atari'),
-      ('God of War', 'Hack n Slash', 'PS2'),
-      ('Mortal Kombat', 'Luta', 'PS2'),
-      ('Valorant', 'FPS', 'PC'),
-      ('Crash Bandicoot', 'Hack n Slash', 'PS2'),
-      ('Need for Speed', 'Corrida', 'PS2'),
+    ('Tetris', 'Puzzle', 'Atari'),
+    ('God of War', 'Hack n Slash', 'PS2'),
+    ('Mortal Kombat', 'Luta', 'PS2'),
+    ('Valorant', 'FPS', 'PC'),
+    ('Crash Bandicoot', 'Hack n Slash', 'PS2'),
+    ('Need for Speed', 'Corrida', 'PS2'),
 ]
 cursor.executemany(jogos_sql, jogos)
 
