e8084766b8522af764717931aef004bfdd368b47
marcsilva <marciliodevjava@gmail.com>
2024-01-17 13:54:40 -0300

    Update views_user.py

---
+++ views_user.py
@@ -0,0 +1,37 @@
+from flask import render_template, request, redirect, session, flash, url_for
+
+from helpers import FormularioUsuario
+from jogoteca import app
+from models import Usuarios
+
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    form = FormularioUsuario()
+    return render_template('login.html', proxima=proxima, form=form)
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    form = FormularioUsuario(request.form)
+    usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
+    if usuario:
+        if form.senha.data == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+        else:
+            flash(f'Senha do {form.nickname.data} esta incoreta!.')
+            return redirect(url_for('login'))
+    else:
+        flash(f'O usuário {form.nickname.data} não existe.')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
